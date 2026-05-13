084f3296d6e0c6e5b93bcc17dbf9e572c291ff86
Stefan <stefan.naeve@hotmail.com>
2026-05-13 23:20:47 +0200

    Condition to Command

---
+++ src/todo/Extensions/CommandExtensions.cs
@@ -0,0 +1,9 @@
+namespace todo.Extensions;
+
+internal static class CommandExtensions {
+    
+    extension(ReadOnlySpan<char> span) {
+        public Command ToCommand() =>
+            Enum.TryParse(span, ignoreCase: true, out Command cond) ? cond : Command.Unknown;
+    }
+}

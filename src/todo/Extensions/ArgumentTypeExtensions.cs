368308cc84eb624d9d03ae4f5c38bda3e7b5f7f5
Stefan <stefan.naeve@hotmail.com>
2026-05-13 23:19:23 +0200

    Update ArgumentTypeExtensions.cs to handle a argument type instead of return the argumentType directly.

--- src/todo/Extensions/ArgumentTypeExtensions.cs
+++ src/todo/Extensions/ArgumentTypeExtensions.cs
@@ -2,23 +2,26 @@ namespace todo.Extensions;
 
 public static class ArgumentTypeExtensions {
     extension(string[] args) {
-        public IEnumerable<ArgumentType> GetArgumentType() {
+        public IEnumerable<Argument> GetArgumentType() {
             foreach (string argument in args) {
-                switch (argument) {
-                    case "-i" or "-important" or "--important":
-                        yield return ArgumentType.Important;
-                        break;
-                    case "-a" or "-all" or "--all":
-                        yield return ArgumentType.All;
-                        break;
-                    case "-info" or "--info":
-                        yield return ArgumentType.Info;
-                        break;
-                    case "-v" or "-verbose" or "--verbose":
-                        yield return ArgumentType.Verbose;
-                        break;
-                }
+                Argument arg = argument switch {
+                    "-i" or "-important" or "--important" =>
+                        new Argument(ArgumentType.Important, null),
+                    "-a" or "-all" or "--all" =>
+                        new Argument(ArgumentType.All, null),
+                    "-info" or "--info" =>
+                        new Argument(ArgumentType.Info, null),
+                    "-v" or "-verbose" or "--verbose" =>
+                        new Argument(ArgumentType.Verbose, null),
+                    string a when !a.StartsWith("-") =>
+                        new Argument(ArgumentType.Value, argument),
+                    _ =>
+                        new Argument(ArgumentType.None, null)
+                };
+                yield return arg;
             }
         }
     }
 }
+
+public record Argument(ArgumentType ArgumentType, string? Value);
